966dcfdd692c85035e8a835c9249289241dc8f56
Virginia Ch <achavarria@universales.com>
2019-03-11 21:47:03 -0600

    todo

--- app.js
+++ app.js
@@ -365,7 +365,7 @@ const callToken = async (authData) => {
     {
   headers: {'Content-Type' : 'application/json' }
   }).then(function (response) {
-        console.log("resultado:" + response);
+        console.log("resultado:" + response.recordset.names);
     })
     .catch(function (error) {
       console.log('ErRo:'+ error.response.headers);
