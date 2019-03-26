5d22b8d64b027605691e007e4ff553880002055c
andycha28 <achavarria@universales.com>
2019-03-26 16:19:15 -0600

    parte de plantillas

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var SimpleDate = require('simple-datejs');
 var config = require('./Global.js');
 const { callSendAPI } = require('./fbApi.js');
 const { sendTextMessage,sendQuickReply } = require('./plantilla.js');
-const { getGMCoti} = require('./GM.js');
+const { gmCoti} = require('./GM.js');
 var detalles ={};
 
 
@@ -576,33 +576,7 @@ await axios.get(urlSaldo,
     });
 }
 
-var gmCoti = {
-  "plan":17,
-  "wayPay":"L4",
-  "group":1,
-  "typeContribution":"N",
-  "startOfValidity":"22/03/2019",
-  "endOfValidity":"22/03/2019",
-  "discount":0.0,
-  "increase":0.0,
-  "discountwws":0.0,
-  "increasewws":0.0,
-  "baseCharge":51,
-  "deductible":0,
-  "typeLife":1,
-  "executive":"LCARDONA",
-  "phone":"",
-  "mail":"",
-  "rate":0.0,
-  "address":"",
-  "dateReception":"22/03/2019",
-  "agent":1,
-  "coin":"01",
-  "contributionBase":0,
-  "ip":"",
-  "observation":"",
-  "message":""
-}
+
 
 
 
@@ -898,8 +872,6 @@ function getcot(grupoId)
 
     for (var x in gmCoti)
     {
-      console.log("valores: "+ gmCoti[x]);
+      console.log(x +": "+ gmCoti[x]);
     }
-
-    
 }
